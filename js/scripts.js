5c5ffbbb1c1c197d881cd9a2663d75fab1eebe47
Nathan Mayer <nm.develops@gmail.com>
2017-07-18 20:19:52 -0700

    NM,FS,MB: Continue on behavior 4, collecting names from inputs

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,42 @@
 // ||      Business Logic         ||
 // ||                             ||
 // =================================
+var p1Fleet = [];
+// var p2Fleet = [];
 
+var Strengths = {
+  carrier: 5,
+  battleship: 4,
+  cruiser: 3,
+  submarine: 3,
+  destroyer: 2
+}
 
+function Ship(type, strength, location, hits) {
+  this.type = type;
+  this.strength = strength;
+  this.location = [];
+  this.hits = 0;
+}
 
+// var newShip = new Ship ($("input#carrier-grid[name=carrier]").val(), )
 
+var gameSetup = (function() {
+  console.log("Is that it...?");
+  $(".P1-inputs input").each(function() {
+    inputtedType = $(this).attr("name");
+    console.log(inputtedType);
+  })
 
+});
+
+var grabGrids = (function() {
+  var testShip = $("input#carrier-grid").val();
+  var shipLocation = testShip.split();
+  console.log(testShip);
+  console.log(shipLocation);
+
+})
 
 
 
@@ -21,3 +52,13 @@
 // ||    User Interface Logic     ||
 // ||                             ||
 // =================================
+$(document).ready(function() {
+  $("button#P1-input-test").click(function() {
+    grabGrids();
+    gameSetup();
+  })
+  $("form#gameSetup").submit(function(event) {
+    prevent.eventDefault();
+
+  })
+})
